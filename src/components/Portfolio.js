ceaa38dd5d2e4fc1e07a55ee52af62a64255e1a7
chelseatroy <chelsea.dommert@gmail.com>
2018-06-27 00:59:36 -0500

    Add the world's ugliest modal

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,17 +1,61 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./Portfolio.css"
 import PropTypes from "prop-types";
+import Modal from 'react-modal';
 
-function Portfolio(props) {
-  return (
-    <div className="contact">
-      <span>{props.name}</span>
-    </div>
-  );
+export default class Portfolio extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {modalIsOpen: false};
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+    }
+
+    afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
+    render() {
+        return (
+            <div className="portfolio-box">
+                <p>{this.props.name}</p>
+                <p>{this.props.value}</p>
+                <button onClick={this.openModal}>Open Modal</button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style={this.customStyles}
+                    contentLabel="Example Modal"
+                >
+
+                    <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                    <button onClick={this.closeModal}>close</button>
+                    <div>I am a modal</div>
+                    <form>
+                        <input/>
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                    </form>
+                </Modal>
+            </div>
+        );
+    }
 }
 
 Portfolio.propTypes = {
-  name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired
 };
-
-export default Portfolio;
